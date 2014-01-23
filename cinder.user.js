ce7ceee19930caed42a923290b65422be7c05ea3
Undo1 <Undo1@users.noreply.github.com>
2014-01-23 15:38:05 -0700

    Update cinder.user.js

--- cinder.user.js
+++ cinder.user.js
@@ -1,1 +1,38 @@
-<placeholder>
+// ==UserScript==
+// @name Script name here
+// @description Short description goes here
+// @namespace Manishearth (This is to avoid conflicts with userscripts that share a name)
+// @author Your name here
+// @license GNU GPL v3 (http://www.gnu.org/copyleft/gpl.html)
+// @include http://stackoverflow.com/*
+// @include http://serverfault.com/*
+// @include http://superuser.com/*
+// @include http://meta.stackoverflow.com/*
+// @include http://meta.serverfault.com/*
+// @include http://meta.superuser.com/*
+// @include http://stackapps.com/*
+// @include http://.stackexchange.com/
+// @include http://askubuntu.com/*
+// @include http://meta.askubuntu.com/*
+// @include http://answers.onstartups.com/*
+// @include http://meta.answers.onstartups.com/*
+// @include http://mathoverflow.net/*
+// @include http://meta.mathoverflow.net/*
+// @include http://discuss.area51.stackexchange.com/*
+// @exclude http://chat./
+
+
+// ==/UserScript==
+function with_jquery(f) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.textContent = "(" + f.toString() + ")(jQuery)";
+    document.body.appendChild(script);
+};
+
+
+
+with_jquery(function($) {
+
+
+});
