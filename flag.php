7800179dd4d21a66f6f176f806ee8fde64779c2d
Undo1 <Undo1@users.noreply.github.com>
2014-01-23 17:36:05 -0700

    Update flag.php

--- flag.php
+++ flag.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
 include "base.php";
 
 $query = mysql_query("SELECT COUNT(*) AS number FROM flags WHERE id=". $id . " ");
