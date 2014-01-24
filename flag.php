7126cfd45028084dbce218812500be6d47a8c5bb
hichris1234 <christopherncarmel@hotmail.com>
2014-01-23 19:28:23 -0500

    Update flag.php

--- flag.php
+++ flag.php
@@ -5,13 +5,14 @@ $query = mysql_query("SELECT COUNT(*) AS number FROM flags WHERE id=". $id . " "
 $inDatabase = mysql_fetch_assoc($query);
 $numInDatabase = $valid["number"];
 
+$site = $_POST["site"];
+$postid = $_POST["postid"];
+$userid = $_POST["userid"];
+
 if($numInDatabase == 0){
-    $site = $_POST["site"];
-    $postid = $_POST["postid"];
-    $userid = $_POST["userid"];
     mysql_query("INSERT INTO flags (Site, PostId, AddDate, UserId, NumFlags, LastFlag)
     VALUES ('" . $site . "', '" . $postid . "', NOW(), '" . $userid . "', 1, NOW())");
 }
 else{
-    mysql_query("UPDATE flags SET NumFlags = NumFlags + 1, LastFlag=NOW() Where Site = NULL AND PostId = NULL");
+    mysql_query("UPDATE flags SET NumFlags = NumFlags + 1, LastFlag=NOW() Where Site = '" . $site ."' AND PostId = '" . $postid . "'");
 }
