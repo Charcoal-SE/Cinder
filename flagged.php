f600fab1d9c1ffd0e7c078e6f80c4e03b486b2a7
Undo1 <parker@erwaysoftware.com>
2014-01-31 08:05:19 -0700

    *bonks self*

--- flagged.php
+++ flagged.php
@@ -6,4 +6,4 @@ include "base.php";
 $stmt = PDODatabaseObject()->query("SELECT * FROM flags WHERE deleted=0");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo json_encode(array('num' => count($query)));
+echo json_encode(array('num' => count($results)));
